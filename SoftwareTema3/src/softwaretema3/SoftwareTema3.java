af9cac78c5dd9e120d308b3375037a29006d3b54
ElielNovelo <56923149+ElielNovelo@users.noreply.github.com>
2020-04-01 20:46:46 -0600

    Empezando Programa

---
+++ SoftwareTema3/src/softwaretema3/SoftwareTema3.java
@@ -0,0 +1,9 @@
+
+package softwaretema3;
+public class SoftwareTema3 {
+    public static void main(String[] args) {
+        JFRPrincipal ventana = new JFRPrincipal();
+        ventana.setVisible(true);
+    }
+    
+}
