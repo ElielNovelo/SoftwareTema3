66018be41e715f1202bedaad2f361900a01b2e8d
GuadalupeKauil <GuadalupeKauil@Eliel-Novelo16>
2020-04-02 17:10:33 -0600

    agregado del metodo de newton

--- SoftwareTema3/src/softwaretema3/CtrlMetodos.java
+++ SoftwareTema3/src/softwaretema3/CtrlMetodos.java
@@ -76,4 +76,44 @@ public class CtrlMetodos {
 
         
     }
+    
+     public tblNewton Newton(double li,double errorDeseado, int it){
+         
+         ArrayList <csNewton> lista = null;
+         tblNewton tabla= null;
+         double errorCalculado = 1;
+         double xAnterior=0;
+        int i = 1;
+        lista = new ArrayList<csNewton>();
+        while (errorCalculado > errorDeseado && it>0) {
+            csNewton fila = new csNewton();
+            fila.setIteracion(i); 
+            fila.setXi(li); 
+            double fxo = this.Funcion(fila.getXi());
+            double fxdo = this.DerivadaFuncion(fila.getXi());
+            fila.setFxi(fxo); 
+            fila.setFdxi(fxdo); 
+            fila.setResta(fila.getXi() - (fxo / fxdo));
+            xAnterior = fila.getXi(); 
+            li = fila.getResta();          
+            if (i > 1) {
+                      
+                errorCalculado = Math.abs((fila.getResta() - fila.getXi()) / fila.getResta());
+                fila.setError(errorCalculado);
+
+            } else {
+                fila.setError(errorCalculado);
+            }
+            lista.add(fila);
+            i++;
+        }
+        if (errorCalculado < errorDeseado) {
+            tabla = new tblNewton(lista);
+            return tabla;
+        } else {
+            return null;
+        }
+         
+         
+     }
 }
